66ad6ce912fac53891650204156f54dbbd30a20d
jian <chenjian@unitechs.com>
2020-08-13 21:57:05 +0800

    猜数字游戏

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -3,18 +3,16 @@ use rand::Rng;
 use std::cmp::Ordering;
 
 fn main() {
-    println!("guess a number");
+    println!("这是一个猜数字游戏");
 
     let rand_num = rand::thread_rng().gen_range(1, 101);
 
     // println!("this random num is :{}", rand_num);
 
     loop {
-        println!("input your number");
+        println!("输入一个数字，以回车结束");
         let mut num_str = String::new();
         io::stdin().read_line(&mut num_str).expect("读取失败,请重启程序");
-
-        println!("your num : {}", num_str);
 
         let num: u32;
 
@@ -28,13 +26,15 @@ fn main() {
             }
         }
 
+        println!("你输入的数字 : {}", num);
+
         let order = num.cmp(&rand_num);
 
         match order {
-            Ordering::Less => println!("your num less"),
-            Ordering::Greater => println!("your num big"),
+            Ordering::Less => println!("你输入的数字小了"),
+            Ordering::Greater => println!("你输入的数字大了"),
             Ordering::Equal => {
-                println!("you win");
+                println!("猜中了，游戏结束");
                 break;
             }
         }
