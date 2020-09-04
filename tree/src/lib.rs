18912ba3e9fd03adde2e6c5488714079b28fd6e2
jian <chenjian@unitechs.com>
2020-09-04 16:33:31 +0800

    other

--- tree/src/lib.rs
+++ tree/src/lib.rs
@@ -30,4 +30,13 @@ mod tests {
 
         tree.for_each(|x| println!("{}", x));
     }
+
+    #[test]
+    fn test2() {
+        let mut tree = TwoTree::new();
+        tree.add("234");
+        tree.add("123");
+        tree.add("8908098");
+        tree.for_each(|x| println!("{}", x));
+    }
 }
